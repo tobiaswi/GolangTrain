701ff518e9e0f547ea4e6fbde9f401e771617871
Tobias Wischmeier <tobias.wischmeier@outlook.com>
2017-07-16 14:33:37 +0200

    working with iota

--- 06_constants/03_iota/main.go
+++ 06_constants/03_iota/main.go
@@ -13,8 +13,51 @@ const (
 	C = iota
 )
 
+const (
+	// D 0
+	D = iota
+	// E 1
+	E = iota
+	// F 2
+	F = iota
+)
+
+const (
+	_ = iota
+	// G 1
+	G = iota
+	// H 2
+	H = iota
+)
+
+const (
+	// throwing away 0
+	_ = iota
+	// KB with iota
+	KB = 1 << (iota * 10)
+	// MB with iota
+	MB = 1 << (iota * 10)
+	// GB with iota
+	GB = 1 << (iota * 10)
+	// TB with iota
+	TB = 1 << (iota * 10)
+)
+
 func main() {
 	fmt.Println(A)
 	fmt.Println(B)
 	fmt.Println(C)
+
+	fmt.Println(D)
+	fmt.Println(E)
+	fmt.Println(F)
+
+	fmt.Println(G)
+	fmt.Println(H)
+
+	fmt.Println("binary\t\tdecimal")
+	fmt.Printf("%b\t%d\n", KB, KB)
+	fmt.Printf("%b\t%d\n", MB, MB)
+	fmt.Printf("%b\t%d\n", GB, GB)
+	fmt.Printf("%b\t%d\n", TB, TB)
 }
